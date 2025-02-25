804d1033539a74be7e60fd4ca17c393c72324973
krishnasingh281 <samar.phone.home@gmail.com>
2025-02-25 21:03:49 +0530

    database configured

--- app/app/settings.py
+++ app/app/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'devdb',
+        'USER': 'devuser',
+        'PASSWORD': 'changeme',
+        'HOST': 'db',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
