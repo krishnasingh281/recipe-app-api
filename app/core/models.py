fa89db59af85f104446cd028c82a02b03c943ac9
krishnasingh281 <samar.phone.home@gmail.com>
2025-03-09 16:11:22 +0530

    setup django admin

--- app/core/models.py
+++ app/core/models.py
@@ -9,11 +9,7 @@ from django.contrib.auth.models import (
 )
 # Create your models here.
 class UserManager(BaseUserManager):
-    
-    """
-    Manager for users.
-    """
-
+    """Manager for users."""
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user."""
         if not email:
@@ -34,10 +30,7 @@ class UserManager(BaseUserManager):
         return user
 
 class User(AbstractBaseUser, PermissionsMixin):
-    
-    """
-    User in the system.
-    """
+    """User in the system."""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -37,7 +37,7 @@ class ModelTests(TestCase):
         """Test that creating a user without an email raises a ValueError."""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user('', 'test123')
-  
+
     def test_create_superuser(self):
         """Test creating a superuser."""
         user = get_user_model().objects.create_superuser(
@@ -47,7 +47,3 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
-
-        
-        
-    
