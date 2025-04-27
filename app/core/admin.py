3923a824415302e040cf88df1174165e7822bbae
krishnasingh281 <samar.phone.home@gmail.com>
2025-04-27 20:32:30 +0530

    final commit

--- app/core/admin.py
+++ app/core/admin.py
@@ -4,8 +4,6 @@ Django admin customization.
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
-
-# Register your models here.
 from core import models
 
 
