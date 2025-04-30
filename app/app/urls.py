e414191b54bf58b5b496a1ce24f968ea8fb2f761
krishnasingh281 <samar.phone.home@gmail.com>
2025-04-30 23:12:43 +0530

    final commit

--- app/app/urls.py
+++ app/app/urls.py
@@ -22,8 +22,6 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/schema/',SpectacularAPIView.as_view(),
-         name='schema'),
-    path('api/docs/',SpectacularSwaggerView.as_view(url_name='schema'),
-         name='docs'),
+    path('api/schema/',SpectacularAPIView.as_view(),name='schema'),
+    path('api/docs/',SpectacularSwaggerView.as_view(url_name='schema'),name='docs'),
 ]
